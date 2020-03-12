d599ca6835da63650c07fb53f632e66c0cc77135
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2020-03-12 18:13:36 -0400

    adding code and test to see single thread

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -9,12 +9,17 @@ class ThreadsTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testUserCanBrowseThreads()
+    public function testUserCanSeeThreads()
     {
         $thread = factory('App\Thread')->create();
-
         $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+    }
 
+    public function testUserCanSeeASingleThread()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads/'.$thread->id);
         $response->assertSee($thread->title);
     }
 }
