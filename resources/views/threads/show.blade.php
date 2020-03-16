742761ea31fca8f9e96f1375e96fc11144ea4eea
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2020-03-16 12:01:02 -0400

    allowing authenticated user to reply to a thread

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,11 +24,19 @@
         </div>
 
         @if(auth()->check())
-            <div class="row justify-content-center">
+            <div class="row justify-content-center" style="margin-top:20px">
                 <div class="col-md-8">
-                    Hello
+                    <form method="POST" action="{{ $thread->path().'/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="reply-body" class="form-control" placeholder="Have something to say..."></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
                 </div>
             </div>
+        @else
+            <div class="text-center" style="margin-top:20px">Please <a href="{{ route('login') }}">sign in</a> to participate in the discussion</div>
         @endif
 
     </div>
