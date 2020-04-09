62b21999c5b2ab7df248760c51079e880806e1e7
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2020-04-09 00:57:13 -0400

    making sure a user can favorite a reply only once and removing commented code

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Favorite;
-use Illuminate\Http\Request;
 use App\Reply;
 
 class FavoritesController extends Controller
@@ -16,26 +14,7 @@ class FavoritesController extends Controller
 
     public function store(Reply $reply)
     {
-        /*return \DB::table('favorites')->insert([
-            'user_id' => auth()->id(),
-            'favorited_id' => $reply->id,
-            'favorited_type' => get_class($reply)
-        ]);*/
-
         return $reply->favorite();
 
-        //return $response;
-
-        /*return Favorite::create([
-            'user_id' => auth()->id(),
-            'favorited_id' => $reply->id,
-            'favorited_type' => 'replies'//get_class($reply)
-        ]);*/
-
-        /*if($response) {
-            return json_encode(true);
-        }
-
-        return json_encode(false);*/
     }
 }
--- tests/Feature/FavoritesTest.php
+++ tests/Feature/FavoritesTest.php
@@ -34,4 +34,21 @@ class FavoritesTest extends TestCase
             'favorited_type' => 'replies',
         ]);
     }
+
+    public function test_an_authenticated_user_can_favorite_a_reply_only_once()
+    {
+        $this->signIn();
+
+        $reply = create('App\Reply');
+
+        try {
+            $this->post("/replies/{$reply->id}/favorites");
+            $this->post("/replies/{$reply->id}/favorites");
+        } catch (\Exception $e) {
+            $this->fail('Did not expect to insert same record twice');
+        }
+
+        $this->assertCount(1, $reply->favorites);
+    }
+
 }
