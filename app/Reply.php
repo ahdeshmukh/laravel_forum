e61795f3cc9888d37752de3690fe89e84b4a32a4
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2020-04-26 00:57:22 -0400

    moving code related to Favorite out of Reply model into Favoritable trait

--- app/Reply.php
+++ app/Reply.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    use Favoritable;
+
     // https://laravel.com/docs/7.x/eloquent#mass-assignment
     protected $guarded = [];
 
@@ -16,27 +18,4 @@ class Reply extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
-
-    public function favorite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-
-        if (!$this->favorites()->where($attributes)->exists()) {
-            return $this->favorites()->create($attributes);
-        }
-    }
-
-    public function isFavorited()
-    {
-        return (bool)$this->favorites->where('user_id', auth()->id())->count();
-    }
-
-    public function getFavoritesCountAttribute()
-    {
-        return $this->favorites->count();
-    }
 }
