5a49e3758b723af4403cd90eda50fb0b8bb2b03b
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2020-05-13 23:14:07 -0400

    adding activity and thread to morphMap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -42,7 +42,9 @@ class AppServiceProvider extends ServiceProvider
         // https://josephsilber.com/posts/2018/07/02/eloquent-polymorphic-relations-morph-map
         // this is to ensure we can store "replies" instead of "App\Reply" in favorited_type column in favorites table
         Relation::morphMap([
-            'replies' => 'App\Reply'
+            'reply' => 'App\Reply',
+            'activity' => 'App\Activity',
+            'thread' => 'App\Thread'
         ]);
     }
 }
